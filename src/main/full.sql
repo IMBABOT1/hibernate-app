9084e7c9d76f245ad33446f99117f359ef2eded3
unknown <zlovred12@gmail.com>
2022-06-26 23:20:44 +0300

    'delete_product'

--- src/main/full.sql
+++ src/main/full.sql
@@ -1,12 +1,12 @@
 DROP TABLE IF EXISTS customers CASCADE;
-CREATE TABLE customers (id bigserial PRIMARY KEY, name VARCHAR(255));
+CREATE TABLE IF NOT EXISTS customers (id bigserial PRIMARY KEY, name VARCHAR(255));
 INSERT INTO customers (name) VALUES
 ('Arnold S.'),
 ('Silvester S.'),
 ('Willis B.');
 
 DROP TABLE IF EXISTS products CASCADE;
-CREATE TABLE products (id bigserial PRIMARY KEY, title VARCHAR(255), price int);
+CREATE TABLE IF NOT EXISTS products (id bigserial PRIMARY KEY, title VARCHAR(255), price int);
 INSERT INTO products (title, price) VALUES
                                             ('box', 10),
                                             ('sphere', 20),
@@ -21,7 +21,15 @@ CREATE TABLE product_buyers (customer_id bigint, product_id bigint, FOREIGN KEY 
 INSERT INTO product_buyers VALUES
 (2, 1),
 (2, 2),
-(2, 3);
+(2, 3),
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(1, 5),
+(1, 5),
+(3, 1);
+
 
 
 
--- src/main/java/ru/geekbrains/hibernate/app/MainApp.java
+++ src/main/java/ru/geekbrains/hibernate/app/MainApp.java
@@ -19,20 +19,86 @@ public class MainApp {
 
         Session session = null;
 
-    try {
-        while (true){
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Press customer id to watch products that client buy");
-            long clientId = scanner.nextInt();
 
-            session = factory.getCurrentSession();
-            session.beginTransaction();
-            Customers customer = session.get(Customers.class, clientId);
+        System.out.println("Press: '/customer' to inspect products that client buy");
+        System.out.println("Press: '/product' to inspect customers that buy certain product");
+        System.out.println("Press: '/break' to exit");
+        System.out.println("Press: '/dell_customer' to delete customer");
+        System.out.println("Press: '/dell_product' to delete customer");
 
-            System.out.println(customer + "\n" + "buy");
-            System.out.println(customer.getProducts());
-        }
-    }catch (Exception e){
+
+
+
+        session = factory.getCurrentSession();
+        session.beginTransaction();
+//        Customers customer1 = session.get(Customers.class, 1);
+//            System.out.println("Loaded item after remove: " + removedSimpleItem);
+//            session.getTransaction().commit()
+
+
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        try {
+            if (name.equals("/customer")){
+                while (true) {
+                Scanner customerId = new Scanner(System.in);
+                System.out.println("Press customer id to watch products that client buy");
+                System.out.println("Press /break to exit");
+                String clientId = customerId.nextLine();
+
+                if (clientId.equals("/break")){
+                    break;
+                }else {
+                    Customers customer = session.get(Customers.class, Long.parseLong(clientId));
+                    System.out.println(customer + "\n" + "buy");
+                    System.out.println(customer.getProducts());
+                }
+            }
+        }else if (name.equals("/product")){
+                while (true) {
+                    Scanner customerId = new Scanner(System.in);
+                    System.out.println("Press product id to watch customers that buy product");
+                    System.out.println("Press /break to exit");
+
+                    String productId = customerId.nextLine();
+
+                    if (productId.equals("/break")) {
+                        break;
+                    } else {
+                        Products product = session.get(Products.class, Long.parseLong(productId));
+                        System.out.println(product + "\n" + "buy");
+                        System.out.println(product.getCustomers());
+                    }
+                }
+            } else if (name.equals("/dell_customer")) {
+                while (true) {
+                    System.out.println("Press customer_id to delete customer");
+                    System.out.println("/break to exit");
+                    Scanner scanner1 = new Scanner(System.in);
+                    String customerId = scanner1.nextLine();
+                    if (customerId.equals("/break")){
+                        break;
+                    }else {
+                        Customers customer = session.get(Customers.class, Long.parseLong(customerId));
+                        session.delete(customer);
+                    }
+                }
+            }else if (name.equals("/dell_product")){
+                while (true) {
+                    System.out.println("Press product_id to delete product");
+                    System.out.println("/break to exit");
+                    Scanner scanner1 = new Scanner(System.in);
+                    String productId = scanner1.nextLine();
+                    if (productId.equals("/break")){
+                        break;
+                    }else {
+                        Products product = session.get(Products.class, Long.parseLong(productId));
+                        session.delete(product);
+                    }
+                }
+            }
+            session.getTransaction().commit();
+        }catch (Exception e){
         e.printStackTrace();
     }finally {
         factory.close();
@@ -40,7 +106,5 @@ public class MainApp {
             session.close();
         }
     }
-
-
     }
 }
