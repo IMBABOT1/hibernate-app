7aed4caf3a9a47fbf5d4de22140dd31b69eead2d
unknown <zlovred12@gmail.com>
2022-06-26 16:45:57 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/hibernate/Customers.java
@@ -0,0 +1,43 @@
+package ru.geekbrains.hibernate;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customers")
+public class Customers {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+
+    public Customers(){
+    }
+
+    public Customers(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("SimpleItem [id = %d, name = %s, price = %d]", id, name);
+    }
+}
